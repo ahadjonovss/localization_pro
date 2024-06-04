ea3383b6382fe61df76e721b79010c81a390ffec
Samandar <samandar.ahadjonov@alif.uz>
2024-06-04 12:18:28 +0500

    example updated

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -10,12 +10,8 @@ void main() {
       locale: const Locale('en', 'US'),
       translations: [
         SupportedTranslation(name: '1', path: 'assets/locales/en_US/1.json'),
-        // Additional translation files for English.
         SupportedTranslation(name: '2', path: 'assets/locales/en_US/2.json'),
         SupportedTranslation(name: '3', path: 'assets/locales/en_US/3.json'),
-        SupportedTranslation(name: '4', path: 'assets/locales/en_US/4.json'),
-        SupportedTranslation(name: '5', path: 'assets/locales/en_US/5.json'),
-        SupportedTranslation(name: '6', path: 'assets/locales/en_US/6.json'),
       ],
     ),
     // Uzbek locale with multiple translation files.
@@ -23,44 +19,32 @@ void main() {
       locale: const Locale('uz', 'UZ'),
       translations: [
         SupportedTranslation(name: '1', path: 'assets/locales/uz_UZ/1.json'),
-        // Additional translation files for Uzbek.
         SupportedTranslation(name: '2', path: 'assets/locales/uz_UZ/2.json'),
         SupportedTranslation(name: '3', path: 'assets/locales/uz_UZ/3.json'),
-        SupportedTranslation(name: '4', path: 'assets/locales/uz_UZ/4.json'),
-        SupportedTranslation(name: '5', path: 'assets/locales/uz_UZ/5.json'),
-        SupportedTranslation(name: '6', path: 'assets/locales/uz_UZ/6.json'),
       ],
     ),
   ];
 
-  // Initialize the localization manager.
-  LocalizationManager locManager = LocalizationManager(
-    supportedLocales: supportedLocales,
-    initialLocale: const Locale('uz', 'UZ'),
-    initialTranslations: ['1'],
-    debugMode: true,
-  );
-
   // Run the application with the localization provider.
-  runApp(MyApp(localizationManager: locManager));
+  runApp(LocalizationProvider(
+      supportedLocales: supportedLocales,
+      initialLocale: const Locale('uz', 'UZ'),
+      initialTranslations: ['1'],
+      debugMode: true,
+      child: MyApp()));
 }
 
 /// The root widget of the application.
 ///
 /// Provides a [LocalizationProvider] to manage localization throughout the application.
 class MyApp extends StatelessWidget {
-  final LocalizationManager localizationManager;
-
-  MyApp({required this.localizationManager});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return LocalizationProvider(
-      localizationManager: localizationManager,
-      child: MaterialApp(
-        title: 'Dynamic Localization Demo',
-        home: HomeScreen(),
-      ),
+    return MaterialApp(
+      title: 'Dynamic Localization Demo',
+      home: HomeScreen(),
     );
   }
 }
@@ -94,50 +78,29 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text('Title: ${'title'.tr()}'), // Translated strings.
-            Text('make.plove.not.war'.tr()), // Translated strings.
-            Text('1:${'one'.tr()}'),
-            Text('2:${'two'.tr()}'),
-            Text('3:${'three'.tr()}'),
-            Text('4:${'four'.tr()}'),
-            Text('5:${'five'.tr()}'),
-            Text('6:${'six'.tr()}'),
+            Text('(1) Title: ${'title'.tr()}'), // Translated strings.
+            Text('(1) ${'make.plove.not.war'.tr()}'), // Translated strings.
+            Text('(1) ${'one'.tr()}'),
             const SizedBox(height: 20),
-            const Text('Adding'), // Section title.
-            // Buttons for adding translations.
+            Text('(2) ${'two'.tr()}'),
+            Text('(2) ${'money'.trPlural(3)}'),
+            const SizedBox(height: 40),
+            Text('adding'.tr()), // Section title.
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 returnAddOrRemoveButton('1'),
                 returnAddOrRemoveButton('2'),
-                returnAddOrRemoveButton('3'),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                returnAddOrRemoveButton('4'),
-                returnAddOrRemoveButton('5'),
-                returnAddOrRemoveButton('6'),
               ],
             ),
             const SizedBox(height: 20),
-            const Text('Removing'), // Section title.
+            Text('removing'.tr()), // Section title.
             // Buttons for removing translations.
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 returnAddOrRemoveButton('1', isAdd: false),
                 returnAddOrRemoveButton('2', isAdd: false),
-                returnAddOrRemoveButton('3', isAdd: false),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                returnAddOrRemoveButton('4', isAdd: false),
-                returnAddOrRemoveButton('5', isAdd: false),
-                returnAddOrRemoveButton('6', isAdd: false),
               ],
             ),
             const SizedBox(height: 40),
@@ -148,12 +111,12 @@ class HomeScreen extends StatelessWidget {
                 ElevatedButton(
                   onPressed: () =>
                       context.changeLocale(const Locale('uz', 'UZ')),
-                  child: const Text('UZBEK'),
+                  child:  Text('uzbek'.tr()),
                 ),
                 ElevatedButton(
                   onPressed: () =>
                       context.changeLocale(const Locale('en', 'US')),
-                  child: const Text('ENGLISH'),
+                  child:  Text('english'.tr()),
                 ),
               ],
             )
